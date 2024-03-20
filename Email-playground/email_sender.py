fb554ddc16d2d802a7b8e60a10b72169267f28d1
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-03-20 02:36:00 -0500

    Add files via upload

---
+++ Email-playground/email_sender.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Hanad Ali'
+email['to'] = 'hajififa@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars'
+
+email.set_content(html.substitute({'name': 'tintin'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('hanadtestemail@gmail.com', 'ghsp nget rhdp kuzr')
+    smtp.send_message(email)
+    print('Sent message')
