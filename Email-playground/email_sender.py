d70cf3366ceb5f9db8b36ef1a2f15fbeeaaf34b0
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-03-20 02:37:55 -0500

    Update email_sender.py

--- Email-playground/email_sender.py
+++ Email-playground/email_sender.py
@@ -6,7 +6,7 @@ from pathlib import Path
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Hanad Ali'
-email['to'] = 'hajififa@gmail.com'
+email['to'] = 'RecipientEmail@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars'
 
 email.set_content(html.substitute({'name': 'tintin'}), 'html')
@@ -14,6 +14,6 @@ email.set_content(html.substitute({'name': 'tintin'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('hanadtestemail@gmail.com', 'ghsp nget rhdp kuzr')
+    smtp.login('Sender@gmail.com', 'SenderPassword')
     smtp.send_message(email)
     print('Sent message')
